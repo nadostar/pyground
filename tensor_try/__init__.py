56a05b644711fdd3a20c05479895fa07d3e0c367
duruwa <sungyee@aliyun.com>
2018-07-25 23:32:59 +0900

    added linear regression, logistic regression

---
+++ tensor_try/__init__.py
@@ -0,0 +1,17 @@
+# Supervised learing
+# - learning with labeled examples - training set
+
+# Unsupervised learning: un-labeled data
+# - Google news grouping
+# - Word clustering
+
+"""
+Types of supervised learning
+
+# Predicting final exam score based on time spent
+  - regression
+# Pass/non-pass based onn time spent
+  - binary classification
+# Letter grade (A,B,C,E and F) based on time spent
+  - multi classification
+"""
